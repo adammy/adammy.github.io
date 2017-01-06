cfe000275234f7eac1ba75541df68c33fafaea67
Adam MacDonald <adam.macdonald-corrao@vistana.com>
2017-01-06 16:43:40 -0500

    Reset of repo and build job

---
+++ README.md
@@ -0,0 +1,2 @@
+# adammyy.github.io
+This is my portfolio website.
--- gruntfile.js
+++ gruntfile.js
@@ -1,25 +1,10 @@
 module.exports = function (grunt) {
 
-	"use strict";
+	'use strict';
 
 	grunt.initConfig({
 
-		pkg: grunt.file.readJSON("package.json"),
-
-		clean: {
-			js: "dist/js/app.js",
-			img: "dist/img/*"
-		},
-
-		concat: {
-			dist: {
-				src: "src/js/**/*.js",
-				dest: "dist/js/app.js",
-				options: {
-					separator: ";"
-				}
-			}
-		},
+		pkg: grunt.file.readJSON('package.json'),
 
 		connect: {
 			server: {
@@ -29,65 +14,47 @@ module.exports = function (grunt) {
 			}
 		},
 
-		imagemin: {
-			dist: {
-				files: [{
-					expand: true,
-					cwd: "src/img/",
-					src: ["**/*.{png,jpg,gif}"],
-					dest: "dist/img/"
-				}]
-			}
-		},
-
 		less: {
 			dist: {
 				options: {
 					compress: true
 				},
 				files: {
-					"dist/css/style.min.css": "src/less/style.less"
+					'css/style.min.css': 'src/less/style.less'
 				}
 			}
 		},
 
 		open: {
 			dev: {
-				path: "http://localhost:8000/"
+				path: 'http://localhost:8000/'
 			}
 		},
 
 		uglify: {
 			dist: {
-				src: "dist/js/app.js",
-				dest: "dist/js/app.min.js"
+				src: 'src/js/app.js',
+				dest: 'js/app.min.js'
 			}
 		},
 
 		watch: {
 			html: {
-				files: ["index.html"],
+				files: ['*.html'],
 				options: {
 					livereload: true
 				}
 			},
 			less: {
-				files: ["src/less/**/*.less"],
-				tasks: ["less"],
+				files: ['src/less/**/*.less'],
+				tasks: ['less'],
 				options: {
 					livereload: true
 				}
 			},
 			js: {
-				files: ["src/js/**/*.js"],
-				tasks: ["concat", "uglify", "clean:js"],
-				options: {
-					livereload: true
-				}
-			},
-			imagemin: {
-				files: ["src/img/**/*.{png,jpg,gif}"],
-				tasks: ["clean:img", "imagemin"],
+				files: ['src/js/**/*.js'],
+				tasks: ['uglify'],
 				options: {
 					livereload: true
 				}
@@ -96,15 +63,12 @@ module.exports = function (grunt) {
 
 	});
 
-	grunt.loadNpmTasks("grunt-contrib-clean");
-	grunt.loadNpmTasks("grunt-contrib-concat");
-	grunt.loadNpmTasks("grunt-contrib-connect");
-	grunt.loadNpmTasks("grunt-contrib-imagemin");
-	grunt.loadNpmTasks("grunt-contrib-less");
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-contrib-watch");
-	grunt.loadNpmTasks("grunt-open");
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-open');
 
-	grunt.registerTask("default", ["clean", "connect", "less", "concat", "uglify", "imagemin", "open", "watch"]);
+	grunt.registerTask('default', ['connect', 'less', 'uglify', 'open', 'watch']);
 
 };
